c0e8b25c6e0e5fcef45287cbe237d45900769464
Mårten Borgström <martenborgstrom@gmail.com>
2020-12-19 13:27:17 +0100

    Day 18

---
+++ dec18/calc.py
@@ -0,0 +1,48 @@
+import aocutils
+import pyparsing as pp
+
+grammar = pp.infixNotation(pp.Word(pp.nums), [
+    (pp.oneOf('* +'), 2, pp.opAssoc.LEFT),
+])
+
+
+def eval_parsed(parsed):
+    left = parsed[0]
+    if isinstance(left, str):
+        val = int(left)
+    else:
+        val = eval_parsed(left)
+    for i in range(1, len(parsed), 2):
+        op = parsed[i]
+        right = parsed[i + 1]
+        if isinstance(right, str):
+            v = int(right)
+        else:
+            v = eval_parsed(right)
+        if op == "*":
+            val *= v
+        elif op == "+":
+            val += v
+        else:
+            assert False
+    return val
+
+
+def evaluate(expr):
+    parsed = grammar.parseString(expr)[0]
+    val = eval_parsed(parsed)
+    return val
+
+
+def main(file):
+    print("RUNNING", file)
+    result = 0
+    for line in aocutils.readlines(file):
+        val = evaluate(line)
+        result += val
+    print(result)
+
+
+if __name__ == '__main__':
+    main("example.txt")
+    main("input.txt")
